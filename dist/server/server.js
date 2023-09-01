1d0a1463fb4ac18372a5d1ea40d4b3b04283c73a
Renee Toscan <renee.toscan@outlook.com>
2023-09-01 11:08:39 -0700

    Server set up and working

--- dist/server/server.js
+++ dist/server/server.js
@@ -1,16 +1,15 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { typeDefs } from "./schema";
-import { Query } from "./resolvers/query";
-// import { ApolloServer }  from '@apollo/server';
-// export const typeDefs = `#graphql
-//   type Query {
-//     hello : String!
-//   }
-// `
-// export const Query = {
-//   hello: () => "Test Success, GraphQL server is up & running !!",
-// };
+// import { typeDefs } from "./schema";
+// import {Query} from "./resolvers/query"
+const typeDefs = `#graphql
+  type Query {
+    hello : String!
+  }
+`;
+const Query = {
+    hello: () => "Test Success, GraphQL server is up & running !!",
+};
 const server = new ApolloServer({
     typeDefs,
     resolvers: {
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,18 +1,17 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { typeDefs } from "./schema";
-import {Query} from "./resolvers/query"
+// import { typeDefs } from "./schema";
+// import {Query} from "./resolvers/query"
 
-// import { ApolloServer }  from '@apollo/server';
 
-// export const typeDefs = `#graphql
-//   type Query {
-//     hello : String!
-//   }
-// `
-// export const Query = {
-//   hello: () => "Test Success, GraphQL server is up & running !!",
-// };
+ const typeDefs = `#graphql
+  type Query {
+    hello : String!
+  }
+`
+ const Query = {
+  hello: () => "Test Success, GraphQL server is up & running !!",
+};
 
 
 const server = new ApolloServer({
