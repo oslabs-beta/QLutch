42f6a6f1a1b537bb311e05b6bd8914a518eeb76e
Renee Toscan <renee.toscan@outlook.com>
2023-09-02 10:49:59 -0700

    Redis server side cache set up

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,31 +1,50 @@
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
-import { mongoose } from "mongoose";
-import fetch from 'node-fetch';
 // import { typeDefs } from "./schema";
 // import {Query} from "./resolvers/query"
+// import { mongoose } from "mongoose";
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import fetch from 'node-fetch';
+import { createClient } from 'redis';
+
+
+//set up redis client
+const client = createClient();
+
+client.on('error', err => console.log('Redis Client Error', err));
+
+await client.connect();
+
+await client.set('redis', 'wheeeee');
+const value = await client.get('redis');
+console.log(value);
 
 const typeDefs = `#graphql
   type Query {
-    people(id: ID!) : People
+    # people(id: ID!) : People
+    hello: String!
+    redis: String
   }
 
-  type People {
-    id: ID!
-    name: String!
-  }
+  # type People {
+  #   id: ID!
+  #   name: String!
+  # }
 
 `;
 const Query = {
-  // hello: () => "Test Success, GraphQL server is up & running !!",
-  people: (parents, args, context) => {
-    const peopleId = args.id;
-    console.log('inside resolver')
+  hello: () => "Test Success, GraphQL server is up & running !!",
+  redis: async () => {
+    const test = await client.get('redis');
+    return test ? test : "item not cached"
+  }
+  // people: (parents, args, context) => {
+  //   const peopleId = args.id;
+  //   console.log('inside resolver')
 
-    fetch(`http://swapi.co/api/people/${peopleId}`)
-      .then((data) => data.json())
-      .then((result) => console.log(result));
-  },
+  //   fetch(`http://swapi.co/api/people/${peopleId}`)
+  //     .then((data) => data.json())
+  //     .then((result) => console.log(result));
+  // },
 };
 
 const server = new ApolloServer({
