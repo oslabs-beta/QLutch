6e941941f897d4c9885469f0e72e0d3860b59f13
Renee Toscan <renee.toscan@outlook.com>
2023-09-06 11:00:29 -0700

    asdf

--- src/server/server.js
+++ src/server/server.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 4000
 const path = require('path');
 const DIST_DIR = path.join(__dirname, '../dist');
 const HTML_FILE = path.join(DIST_DIR, 'index.html');
+const redis = require("./redis");
 
 const app = express();
 app.use(express.static(DIST_DIR));
