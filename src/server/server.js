a4ace0938bda0d40de1d71fc1ed840d430bd4f3e
Renee Toscan <renee.toscan@outlook.com>
2023-09-06 19:40:07 -0700

    Fixed Renee's "fetch is not defined" error

--- src/server/server.js
+++ src/server/server.js
@@ -9,6 +9,7 @@ const path = require('path');
 const DIST_DIR = path.join(__dirname, '../dist');
 const HTML_FILE = path.join(DIST_DIR, 'index.html');
 
+
 const app = express();
 app.use(express.static(DIST_DIR));
 app.use(cors());
